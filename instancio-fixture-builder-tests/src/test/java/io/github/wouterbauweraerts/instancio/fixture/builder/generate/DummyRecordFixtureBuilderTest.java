53688db4aa35fb3a908b8238b388ba95af3675ae
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2025-02-03 16:24:34 +0100

    #8: add tests to check expected methods exist. Including failing test for toFixtureBuiler(T t) method that has to be created

--- instancio-fixture-builder-tests/src/test/java/io/github/wouterbauweraerts/instancio/fixture/builder/generate/DummyRecordFixtureBuilderTest.java
+++ instancio-fixture-builder-tests/src/test/java/io/github/wouterbauweraerts/instancio/fixture/builder/generate/DummyRecordFixtureBuilderTest.java
@@ -1,11 +1,19 @@
 package io.github.wouterbauweraerts.instancio.fixture.builder.generate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.lang.reflect.Method;
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 import io.github.wouterbauweraerts.instancio.fixture.builder.dummy.ContactDetails;
 import io.github.wouterbauweraerts.instancio.fixture.builder.dummy.ContactDetailsFixtures;
@@ -29,5 +37,48 @@ class DummyRecordFixtureBuilderTest {
                         .build()
         ).usingRecursiveComparison()
                 .isEqualTo(new DummyRecord(name, firstName, birthDate, reliabilityScore, contactDetails));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> hasExpectedParameterlessMethods() {
+        return Stream.of(
+                "self",
+                "build",
+                "ignoreFirstName",
+                "ignoreName",
+                "ignoreBirthDate",
+                "ignoreContactDetails",
+                "ignoreReliabilityScore"
+        ).map(name -> DynamicTest.dynamicTest(name, () -> {
+            assertAll(() -> {
+                AtomicReference<Method> methodReference = new AtomicReference<>();
+
+                assertThatCode(() -> methodReference.set(getMethod(DummyRecordFixtureBuilder.class, name))).doesNotThrowAnyException();
+                assertThat(methodReference.get()).isNotNull();
+            });
+        }));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> hasExpectedMethodsWithParameters() {
+        return Map.of(
+                "withName", new Class<?>[]{String.class},
+                "withFirstName", new Class<?>[]{String.class},
+                "withBirthDate", new Class<?>[]{LocalDate.class},
+                "withReliabilityScore", new Class<?>[]{Integer.class},
+                "withContactDetails", new Class<?>[]{ContactDetails.class},
+                "toFixtureBuilder", new Class[]{DummyRecord.class}
+        ).entrySet().stream().map(entry -> DynamicTest.dynamicTest(entry.getKey(), () -> {
+            assertAll(() -> {
+                AtomicReference<Method> methodReference = new AtomicReference<>();
+
+                assertThatCode(() -> methodReference.set(getMethod(DummyRecordFixtureBuilder.class, entry.getKey(), entry.getValue()))).doesNotThrowAnyException();
+                assertThat(methodReference.get()).isNotNull();
+            });
+        }));
+    }
+
+    private Method getMethod(Class<?> clazz, String name, Class<?>... parameterTypes) throws NoSuchMethodException {
+        return clazz.getMethod(name, parameterTypes);
     }
 }
